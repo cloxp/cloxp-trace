4e0412fcbcdaa8a338cca5b70ae790ede5e95fd6
Robert Krahn <robert.krahn@gmail.com>
2015-03-29 17:26:45 -0700

    license

--- project.clj
+++ project.clj
@@ -1,8 +1,10 @@
 (defproject org.rksm/cloxp-trace "0.1.4-SNAPSHOT"
   :description "Tracing for cloxp."
-  :license "MIT"
+  :license {:name "MIT License"
+            :url "http://opensource.org/licenses/MIT"}
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [org.clojure/data.json "0.2.3"]
+                 [org.clojure/tools.trace "0.7.8"]
                  [clj-stacktrace/clj-stacktrace "0.2.8"]
                  [org.rksm/cloxp-source-reader "0.1.0-SNAPSHOT"]]
   :source-paths ["src/main"]
