fea654fe300c51876817a9420a9f4c7e4cb20dc6
Robert Krahn <robert.krahn@gmail.com>
2015-04-11 06:52:26 -0700

    reader update

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.rksm/cloxp-trace "0.1.4"
+(defproject org.rksm/cloxp-trace "0.1.5-SNAPSHOT"
   :description "Tracing for cloxp."
   :license {:name "MIT License"
             :url "http://opensource.org/licenses/MIT"}
@@ -6,7 +6,7 @@
                  [org.clojure/data.json "0.2.3"]
                  [org.clojure/tools.trace "0.7.8"]
                  [clj-stacktrace/clj-stacktrace "0.2.8"]
-                 [org.rksm/cloxp-source-reader "0.1.1"]]
+                 [org.rksm/cloxp-source-reader "0.1.2-SNAPSHOT"]]
   :source-paths ["src/main"]
   :test-paths ["src/test"]
   :scm {:url "git@github.com:cloxp/cloxp-trace.git"}
