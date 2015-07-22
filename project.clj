cbf1d64fe2b07336f3aba77f504be7714ca0b6cd
Robert Krahn <robert.krahn@gmail.com>
2015-07-22 14:41:04 -0700

    version bump

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.rksm/cloxp-trace "0.1.9"
+(defproject org.rksm/cloxp-trace "0.1.10-SNAPSHOT"
   :description "Tracing for cloxp."
   :license {:name "MIT License"
             :url "http://opensource.org/licenses/MIT"}
