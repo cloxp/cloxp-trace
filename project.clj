640d6245bdbeabcdba589dbbaa918481147fc368
Robert Krahn <robert.krahn@gmail.com>
2015-07-22 14:45:27 -0700

    lib update

--- project.clj
+++ project.clj
@@ -6,7 +6,7 @@
                  [org.clojure/data.json "0.2.6"]
                  [org.clojure/tools.trace "0.7.8"]
                  [clj-stacktrace/clj-stacktrace "0.2.8"]
-                 [org.rksm/cloxp-source-reader "0.1.8"]
+                 [org.rksm/cloxp-source-reader "0.1.9-SNAPSHOT"]
                  [medley "0.7.0"]]
   :source-paths ["src/main"]
   :test-paths ["src/test"]
